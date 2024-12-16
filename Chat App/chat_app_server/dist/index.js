4f812ede34d04559a370ed04d6ee14831136f06a
rohits-web03 <rohit.dey302003@gmail.com>
2024-12-17 04:02:35 +0530

    Added chatting in room logic

--- Chat App/chat_app_server/dist/index.js
+++ Chat App/chat_app_server/dist/index.js
@@ -3,20 +3,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let userCount = 0;
-let allSockets = [];
+let allSockets = new Map();
+/*
+{
+   "type": "join",
+   "payload": {
+     "roomId": "123"
+   }
+}
+{
+    "type": "chat",
+    "payload": {
+        "message: "hi there",
+        "roomId": "123"
+    }
+}
+*/
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount += 1;
-    const userId = userCount;
-    console.log(`User connected #${userId}`);
-    socket.on("message", (message) => {
-        console.log(`${message} received from #${userId}`);
-        allSockets.forEach((client) => {
-            client.send(`#${userId}:${message} from server`);
-        });
+    socket.on("message", (msg) => {
+        var _a;
+        let message = JSON.parse(msg.toString());
+        if (message.type === "join") {
+            const room = message.payload.roomId;
+            if (!allSockets.has(room)) {
+                allSockets.set(room, []);
+            }
+            (_a = allSockets.get(room)) === null || _a === void 0 ? void 0 : _a.push(socket);
+        }
+        if (message.type === "chat") {
+            const room = message.payload.roomId;
+            const roomUsers = allSockets.get(room);
+            roomUsers === null || roomUsers === void 0 ? void 0 : roomUsers.forEach((user) => {
+                user.send(message.payload.message);
+            });
+        }
     });
     socket.on("close", () => {
         userCount -= 1;
-        console.log(`User disconnected #${userId}`);
+        console.log(`User disconnected`);
     });
 });
--- Chat App/chat_app_server/src/index.ts
+++ Chat App/chat_app_server/src/index.ts
@@ -3,23 +3,29 @@ import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
-let allSockets: WebSocket[] =[]
+let allSockets: Map<string,WebSocket[]> =new Map();
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount += 1;
-    const userId = userCount;
-    console.log(`User connected #${userId}`);
-
-    socket.on("message", (message)=>{
-        console.log(`${message} received from #${userId}`);
-        allSockets.forEach((client)=>{
-            client.send(`#${userId}:${message} from server`);
-        })
+    socket.on("message", (msg)=>{
+        let message=JSON.parse(msg.toString());
+        if(message.type==="join"){
+            const room=message.payload.roomId;
+            if(!allSockets.has(room)){
+                allSockets.set(room,[]);
+            }
+            allSockets.get(room)?.push(socket);
+        }
+        if(message.type==="chat"){
+            const room=message.payload.roomId;
+            const roomUsers=allSockets.get(room);
+            roomUsers?.forEach((user)=>{
+                user.send(message.payload.message);
+            })
+        }
     })
 
     socket.on("close", () => {
-        userCount-=1;
-        console.log(`User disconnected #${userId}`);
+        // userCount-=1;
+        console.log(`User disconnected`);
     });
 });
